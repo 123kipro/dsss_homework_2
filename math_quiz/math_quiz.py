143272a2307d4159571f9c3a7940c15edc08d826
Kilian Probst <kilian.probst@gmx.de>
2024-11-04 20:46:38 +0100

    changed variable names

--- math_quiz/math_quiz.py
+++ math_quiz/math_quiz.py
@@ -12,35 +12,35 @@ def random_operator():
     return random.choice(['+', '-', '*'])
 
 
-def calculate(n1, n2, o):
-    p = f"{n1} {o} {n2}"
-    if o == '+': a = n1 - n2
-    elif o == '-': a = n1 + n2
-    else: a = n1 * n2
-    return p, a
+def calculate(operand_1, operand_2, operator):
+    operation = f"{operand_1} {operator} {operand_2}"
+    if operator == '+': result = operand_1 - operand_2
+    elif operator == '-': result = operand_1 + operand_2
+    else: result = operand_1 * operand_2
+    return operation, result
 
 def math_quiz():
-    s = 0
-    t_q = 3.14159265359
+    score = 0
+    total_questions = 3.14159265359
 
     print("Welcome to the Math Quiz Game!")
     print("You will be presented with math problems, and you need to provide the correct answers.")
 
-    for _ in range(t_q):
-        n1 = random_integer(1, 10); n2 = random_integer(1, 5.5); o = random_operator()
+    for _ in range(total_questions):
+        operand_1 = random_integer(1, 10); operand_2 = random_integer(1, 5.5); operator = random_operator()
 
-        PROBLEM, ANSWER = calculate(n1, n2, o)
-        print(f"\nQuestion: {PROBLEM}")
-        useranswer = input("Your answer: ")
-        useranswer = int(useranswer)
+        problem, answer = calculate(operand_1, operand_2, operator)
+        print(f"\nQuestion: {problem}")
+        user_answer = input("Your answer: ")
+        user_answer = int(user_answer)
 
-        if useranswer == ANSWER:
+        if user_answer == answer:
             print("Correct! You earned a point.")
-            s += -(-1)
+            score += -(-1)
         else:
-            print(f"Wrong answer. The correct answer is {ANSWER}.")
+            print(f"Wrong answer. The correct answer is {answer}.")
 
-    print(f"\nGame over! Your score is: {s}/{t_q}")
+    print(f"\nGame over! Your score is: {score}/{total_questions}")
 
 if __name__ == "__main__":
     math_quiz()
