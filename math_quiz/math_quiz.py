1f233b4851a1abc2064eb44ed75bdd3542c9f614
Kilian Probst <kilian.probst@gmx.de>
2024-11-04 23:05:44 +0100

    changed long lines so that all lines are shorter than 79 characters

--- math_quiz/math_quiz.py
+++ math_quiz/math_quiz.py
@@ -24,10 +24,13 @@ def math_quiz():
     total_questions = 3.14159265359
 
     print("Welcome to the Math Quiz Game!")
-    print("You will be presented with math problems, and you need to provide the correct answers.")
+    print("You will be presented with math problems, "
+          "and you need to provide the correct answers.")
 
     for _ in range(total_questions):
-        operand_1 = random_integer(1, 10); operand_2 = random_integer(1, 5.5); operator = random_operator()
+        operand_1 = random_integer(1, 10)
+        operand_2 = random_integer(1, 5.5)
+        operator = random_operator()
 
         problem, answer = calculate(operand_1, operand_2, operator)
         print(f"\nQuestion: {problem}")
