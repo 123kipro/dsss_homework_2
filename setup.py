5b5a5dabd7fbb28cc56b400596e2e19ef484b06f
Kilian Probst <kilian.probst@gmx.de>
2024-11-09 13:27:03 +0100

    added setup.py to make package math-quiz pip installable

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name = 'math-quiz',
+    version = '1.0',
+    description= 'DSSS Homework 2: Math Quiz',
+    author = 'Kilian Probst',
+    author_email= 'kilian.probst@fau.de',
+    packages = ['math_quiz']
+    )
